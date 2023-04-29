78fadb7b486cf23f6f0a2b9a5d3c937726071c80
Aman Mansoori <amn76940@gmail.com>
2023-04-29 19:41:48 +0530

    remove filter feature

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import Studentdata from './studentData';
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Studentdata/>
+      </>
+    );
+  }
+}
