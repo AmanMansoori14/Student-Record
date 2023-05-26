f48d0047e6550ad2e108c34079d9edaecddb158b
Aman Mansoori <amn76940@gmail.com>
2023-05-26 16:42:14 +0530

    Added Search feature

--- studentData.jsx
+++ studentData.jsx
@@ -50,6 +50,16 @@ export default class Studentdata extends Component {
     this.setState({ filterBy: branch })
   }
 
+  searchRecord = (e) => {
+    let search = this.searchBox.value;
+
+    this.setState({
+      filterBy: search
+    });
+
+    e.preventDefault();
+  }
+
   render() {
     return (
       <>
@@ -60,8 +70,7 @@ export default class Studentdata extends Component {
               <div className="col-xl-4 mt-2">
                 <input
                   type="number"
-                  onFocus={() => this.setState({ isRollDuplicate: false })}
-                  onBlur={this.checkRoll}
+                  onChange={this.checkRoll}
                   className="form-control"
                   ref={(ob) => (this.rollBox = ob)}
                   placeholder="Roll no"
@@ -75,7 +84,7 @@ export default class Studentdata extends Component {
               <div className="col-xl-4 mt-2">
                 <select className="form-control" ref={(ob) => (this.branchList = ob)} required>
                   <option value="">Select Branch</option>
-                  {this.branches.map(br => <option>{br}</option>)}
+                  {this.branches.map(br => <option key={br}>{br}</option>)}
                 </select>
               </div>
             </div>
@@ -103,19 +112,25 @@ export default class Studentdata extends Component {
                 </b>
                 &nbsp;
                 {this.branches.map(
-                  br => <b onClick={this.filterStd} data-branch={br} className='btn btn-info m-1'> {br} {this.state.students.reduce((a, i) => (i.branch === br) ? a + 1 : a, 0)}</b>
+                  br => <b onClick={this.filterStd} data-branch={br} className='btn btn-info m-1' key={br}> {br} {this.state.students.reduce((a, i) => (i.branch === br) ? a + 1 : a, 0)}</b>
                 )}
               </div>
             </div>
           </form>
-          <div className="col-xl-6 mt-2">
-            <b className="text-danger">
-              {this.state.isRollDuplicate ? "Roll number is already exist" : ""}
-            </b>
+          <div className="row mt-3">
+            <div className="col-xl-9">
+              <b className="text-danger">
+                {this.state.isRollDuplicate ? "Roll number is already exist" : ""}
+              </b>
+            </div>
+            <div className="col-xl-3 text-right">
+              <input className="form-control" ref={ob => this.searchBox = ob} onInput={() => (this.searchBox.value === "" ? this.setState({ filterBy: undefined }) : false)} placeholder="Enter to Search"></input>
+              <button onClick={this.searchRecord} className='btn btn-primary btn-sm mt-1'>Search</button>
+            </div>
           </div>
         </div>
 
-        <div className="container-fluid mt-3">
+        <div className="container mt-3">
           <table className="table">
             <thead>
               <tr>
@@ -132,26 +147,26 @@ export default class Studentdata extends Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.students.filter((ob) => {
-                return (this.state.filterBy === undefined) ? true : ob.branch === this.state.filterBy;
-              }).map((elem, i) => {
-                return (
-                  <tr>
-                    <td>{i + 1}</td>
-                    <td>{elem.roll}</td>
-                    <td>{elem.name}</td>
-                    <td>{elem.branch}</td>
-                    <td>{elem.physics}</td>
-                    <td>{elem.chemistry}</td>
-                    <td>{elem.maths}</td>
-                    <td>{elem.physics + elem.chemistry + elem.maths}</td>
-                    <td>{((elem.physics + elem.chemistry + elem.maths) / 3).toFixed(2)}%</td>
-                    <td>
-                      <button onClick={this.deleteStd} data-roll={elem.roll} className='btn btn-danger'>Delete</button>
-                    </td>
-                  </tr>
-                );
-              })}
+              {(this.state.students.length === 0 || this.state.students.length === "") ?
+                <tr> <td className="text-center" colSpan={10}>No Record found</td></tr> :
+                this.state.students.filter((ob) => {
+                  return (this.state.filterBy === undefined) ? true : ob.branch === this.state.filterBy || ob.roll === this.state.filterBy * 1 || ob.name === this.state.filterBy;
+                }).map((elem, i) => {
+                  return (
+                    <tr><td>{i + 1}</td>
+                      <td>{elem.roll}</td>
+                      <td>{elem.name}</td>
+                      <td>{elem.branch}</td>
+                      <td>{elem.physics}</td>
+                      <td>{elem.chemistry}</td>
+                      <td>{elem.maths}</td>
+                      <td>{elem.physics + elem.chemistry + elem.maths}</td>
+                      <td>{((elem.physics + elem.chemistry + elem.maths) / 3).toFixed(2)}%</td>
+                      <td>
+                        <button onClick={this.deleteStd} data-roll={elem.roll} className='btn btn-danger'>Delete</button>
+                      </td></tr>
+                  );
+                })}
             </tbody>
           </table>
         </div>
